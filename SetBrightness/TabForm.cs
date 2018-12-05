99e63f1be91cd335159a2c3761002b88a36bfbd4
leto <1907984744@qq.com>
2018-12-06 03:42:49 +0800

    fix set action delay

--- SetBrightness/TabForm.cs
+++ SetBrightness/TabForm.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Win32;
--- SetBrightness/TabPageTemplate.cs
+++ SetBrightness/TabPageTemplate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Threading;
@@ -11,6 +12,7 @@ namespace SetBrightness
     {
         private readonly Monitor _monitor;
         private readonly MonitorsManager _monitorsManager;
+        private readonly SetMonitorQueue _setQueue;
         public readonly MonitorType MonitorType;
 
         public TabPageTemplate(Monitor monitor, string name, MonitorsManager monitorsManager)
@@ -20,6 +22,8 @@ namespace SetBrightness
             _monitorsManager = monitorsManager;
             Name = name;
             MonitorType = monitor.Type;
+
+            _setQueue = new SetMonitorQueue(_monitor);
             PreWork();
         }
 
@@ -43,12 +47,12 @@ namespace SetBrightness
             brightTrackbar.ValueChanged += (sender, e) =>
             {
                 var value = brightTrackbar.Value;
-                new Thread(() => _monitor.SetBrightness(value)).Start();
+                new Thread(() => _setQueue.Add(value, true)).Start();
             };
             contrastTrackbar.ValueChanged += (sender, e) =>
             {
                 var value = contrastTrackbar.Value;
-                new Thread(() => _monitor.SetContrast(value)).Start();
+                new Thread(() => _setQueue.Add(value, false)).Start();
             };
         }
 
@@ -84,7 +88,7 @@ namespace SetBrightness
                 }
 
                 brightTrackbar.Value = value;
-                new Thread(() => { _monitor.SetBrightness(value); }).Start();
+                new Thread(() => { _setQueue.Add(value, true); }).Start();
             }
         }
 
@@ -98,8 +102,8 @@ namespace SetBrightness
             try
             {
                 await Task.WhenAll(
-                    Task.Run((() => SafeSetTrackBar(brightTrackbar, _monitor.GetBrightness()))),
-                    Task.Run((() => SafeSetTrackBar(contrastTrackbar, _monitor.GetContrast()))));
+                    Task.Run(() => SafeSetTrackBar(brightTrackbar, _monitor.GetBrightness())),
+                    Task.Run(() => SafeSetTrackBar(contrastTrackbar, _monitor.GetContrast())));
             }
             catch (InvalidMonitorException e)
             {
@@ -112,11 +116,50 @@ namespace SetBrightness
         {
             if (trackBar.InvokeRequired)
             {
-                this.Invoke(new Action<TrackBar, int>(SafeSetTrackBar), trackBar, value);
+                Invoke(new Action<TrackBar, int>(SafeSetTrackBar), trackBar, value);
             }
             else
             {
                 trackBar.Value = value;
+            }
+        }
+
+        // kind like producer/customer model ?
+        private class SetMonitorQueue
+        {
+            private readonly Queue<int> _brightnessQueue = new Queue<int>();
+            private readonly Queue<int> _contrastQueue = new Queue<int>();
+            private readonly object _brightnessLock = new object();
+            private readonly object _contrastLock = new object();
+            private readonly Monitor _monitor;
+
+            public SetMonitorQueue(Monitor monitor)
+            {
+                _monitor = monitor;
+            }
+
+            public void Add(int value, bool isBrightness)
+            {
+                var @lock = isBrightness ? _brightnessLock : _contrastLock;
+                var queue = isBrightness ? _brightnessQueue : _contrastQueue;
+                var action = isBrightness ? (Action<int>) _monitor.SetBrightness : _monitor.SetContrast;
+                if (queue.Count > 0)
+                {
+                    queue.Clear();
+                }
+
+                queue.Enqueue(value);
+
+                lock (@lock)
+                {
+                    if (queue.Count == 0)
+                    {
+                        return;
+                    }
+
+                    var des = queue.Dequeue();
+                    action.Invoke(des);
+                }
             }
         }
     }
